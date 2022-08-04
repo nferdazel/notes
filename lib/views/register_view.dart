3809859c2e5fdd043d21dcc445216bda68455cf4
Fredianto <fredinix@proton.me>
2022-08-04 20:44:06 +0700

    error handling in register view

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -4,6 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/constants/routes.dart';
 
+import '../utilities/error_dialog.dart';
+
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
 
@@ -65,13 +67,9 @@ class _RegisterViewState extends State<RegisterView> {
 
                 dev.log(credential.toString());
               } on FirebaseAuthException catch (e) {
-                if (e.code == 'weak-password') {
-                  dev.log('weak password');
-                } else if (e.code == 'email-already-in-use') {
-                  dev.log('email already in use');
-                } else if (e.code == 'invalid-email') {
-                  dev.log('invalid email');
-                }
+                await errorDialog(context, e.message.toString());
+              } catch (e) {
+                await errorDialog(context, e.toString());
               }
             },
             child: const Text('Register'),
