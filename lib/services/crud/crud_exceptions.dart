ef04a21b9f5e316075c8e4ef693f190f3887abff
Fredianto <fredinix@proton.me>
2022-08-10 12:42:09 +0700

    feat(db): setup and configure sqlite query functions

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenedException implements Exception {}
+
+class DatabaseIsNotOpened implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotFindNote implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
+
+class UserAlreadyExists implements Exception {}
