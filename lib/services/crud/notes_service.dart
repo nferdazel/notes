18d6b7cbb19dd7fbae081075f25c55c53e0e91c8
Fredianto <fredinix@proton.me>
2022-08-12 22:37:44 +0700

    fix(db): updateNote is updating all rows

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -91,10 +91,15 @@ class NotesService {
     await getNote(id: note.id);
 
     // update the db
-    final updatesCount = await db.update(noteTable, {
-      textColumn: text,
-      isSyncedWithCloudColumn: 0,
-    });
+    final updatesCount = await db.update(
+      noteTable,
+      {
+        textColumn: text,
+        isSyncedWithCloudColumn: 0,
+      },
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
 
     if (updatesCount == 0) {
       throw CouldNotUpdateNote();
