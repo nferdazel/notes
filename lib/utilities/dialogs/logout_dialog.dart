f84018fa477ae24293c35737930bf4f4018a4921
Fredianto <fredinix@proton.me>
2022-08-12 19:00:51 +0700

    generic_dialog and its implementations

---
+++ lib/utilities/dialogs/logout_dialog.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:notes/utilities/dialogs/generic_dialog.dart';
+
+Future<bool> showLogoutDialog(BuildContext context) {
+  return showGenericDialog<bool>(
+    context: context,
+    title: 'Logout',
+    content: 'Are you sure you want to logout?',
+    optionsBuilder: () => {'Cancel': false, 'Logout': true},
+  ).then((value) => value ?? false);
+}
